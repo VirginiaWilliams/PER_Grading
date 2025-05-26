820799b467dd51e19d6c22add8226dabdf2383b7
VirginiaWilliams <ginnyglink@gmail.com>
2025-05-26 14:17:48 -0400

    removed old version

--- PER Grader-win32-x64/resources/app/renderer/questions/PER17Questions.js
+++
@@ -1,6 +0,0 @@
-import { InputCell } from "../types/inputCell.js";
-
-export const PER17Reading = [
-    new InputCell('test','6'),
-    new InputCell('test2','4')
-]
--- PER Grader-win32-x64/resources/app/renderer/types/inputCell.js
+++
@@ -1,6 +0,0 @@
-export class InputCell {
-    constructor(label, total) {
-        this.label = label;
-        this.total = total;
-    }
-}
